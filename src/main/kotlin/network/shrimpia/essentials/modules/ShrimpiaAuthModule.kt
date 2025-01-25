af08915ecf285a8d216febcfe2b3b61cfe196de8
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2025-01-25 16:39:36 +0900

    ログイン時の認証が2度実行されてしまう不具合を修正するために、AsyncPlayerPreLoginEventを使用

--- src/main/kotlin/network/shrimpia/essentials/modules/ShrimpiaAuthModule.kt
+++ src/main/kotlin/network/shrimpia/essentials/modules/ShrimpiaAuthModule.kt
@@ -9,7 +9,7 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerLoginEvent
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent
 import org.json.simple.JSONObject
 import org.json.simple.parser.JSONParser
 
@@ -32,49 +32,70 @@ class ShrimpiaAuthModule : ModuleBase(), Listener {
         apiKey = config.getString("portal_api_key") ?: throw IllegalStateException("portal_api_key is not set at config.yml")
         apiUrl = config.getString("portal_api_url") ?: throw IllegalStateException("portal_api_url is not set at config.yml")
     }
-    @EventHandler
+
     /**
      * プレイヤーの参加時に呼び出される
      */
-    fun onPlayerLogin(ev: PlayerLoginEvent) {
+    @EventHandler
+    fun onPlayerLogin(ev: AsyncPlayerPreLoginEvent) {
+        // 既にログインが弾かれている場合は処理を行わない
+        if (ev.loginResult != AsyncPlayerPreLoginEvent.Result.ALLOWED) {
+            return
+        }
+
         // プレイヤーIDと名前から、リクエストのbodyを生成
-        val root = JSONObject();
-        root["playerId"] = ev.player.uniqueId.toString()
-        root["playerName"] = ev.player.name
+        val root = JSONObject()
+        root["playerId"] = ev.uniqueId.toString()
+        root["playerName"] = ev.name
         val body = root.toJSONString().toRequestBody("application/json".toMediaType())
 
         // リクエストを生成
         val req = Request.Builder()
-            .url("$apiUrl/internal-auth")
+            .url("$apiUrl/minecraft/internal-auth")
             .method("POST", body)
             .header("X-Internal-Api-Key", apiKey)
-            .build();
+            .build()
 
-        // APIリクエストを送信
-        http.newCall(req).execute().use { res ->
-            // レスポンスが返ってきたあとの処理
+        try {
+            // APIリクエストを送信
+            http.newCall(req).execute().use { res ->
+                // レスポンスが返ってきたあとの処理
+                val resBody = res.body?.string()
+                // APIが異常であれば、内部エラーとして扱う
+                if (!res.isSuccessful || resBody == null) {
+                    ev.disallow(
+                        AsyncPlayerPreLoginEvent.Result.KICK_OTHER,
+                        Component.text("ポータルとの接続に失敗しました。皇帝へお問い合わせください。")
+                    )
+                    plugin.logger.severe("シュリンピアポータルへのリクエストに失敗しました。")
+                    plugin.logger.severe("status:${res.code} body:${resBody ?: "null"}")
+                    return
+                }
 
-            val resBody = res.body?.string()
-            // APIが異常であれば、内部エラーとして扱う
-            if (!res.isSuccessful || resBody == null) {
-                ev.disallow(PlayerLoginEvent.Result.KICK_OTHER, Component.text("ポータルとの接続に失敗しました。皇帝へお問い合わせください。"))
-                return
+                // レスポンスのJSONをパース
+                val data = JSONParser().parse(resBody) as JSONObject
+                val ok = data["ok"] as Boolean
+
+                // 認証が通っていれば、そのままログインを許可
+                if (ok) {
+                    ev.allow()
+                    plugin.logger.info("プレイヤー ${ev.name} は、ポータル認証に成功しました。")
+                    return
+                }
+
+                // 通っていなければ、メッセージをそのまま表示する
+                val message = data["error"] as String
+                ev.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, Component.text(message))
+                plugin.logger.info("プレイヤー ${ev.name} は、ポータル認証に失敗しました。")
             }
-
-            // レスポンスのJSONをパース
-            val data = JSONParser().parse(resBody) as JSONObject
-            val ok = data["ok"] as Boolean
-
-            // 認証が通っていれば、そのままログインを許可
-            if (ok) {
-                ev.allow()
-                return
-            }
-
-            // 通っていなければ、メッセージをそのまま表示する
-            val message = data["message"] as String
-            ev.disallow(PlayerLoginEvent.Result.KICK_OTHER, Component.text(message))
+        } catch (exception: Exception) {
+            // 例外が発生した場合は、内部エラーとして扱う
+            ev.disallow(
+                AsyncPlayerPreLoginEvent.Result.KICK_OTHER,
+                Component.text("ポータルとの接続に失敗しました。皇帝へお問い合わせください。")
+            )
+            plugin.logger.severe("シュリンピアポータルへのリクエスト時に例外が発生しました。")
+            plugin.logger.severe("${exception.javaClass.simpleName}: ${exception.message}\n${exception.stackTrace.joinToString("\n")}")
         }
-
     }
 }
