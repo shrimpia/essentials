8201fd3d32f3a13832c853bda341a5b66f8d3688
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2025-01-27 15:41:11 +0900

    参加時の表示を改善

--- src/main/kotlin/network/shrimpia/essentials/modules/GreetingModule.kt
+++ src/main/kotlin/network/shrimpia/essentials/modules/GreetingModule.kt
@@ -1,5 +1,7 @@
 package network.shrimpia.essentials.modules
 
+import io.papermc.paper.registry.keys.SoundEventKeys
+import net.kyori.adventure.sound.Sound
 import net.kyori.adventure.title.Title
 import network.shrimpia.essentials.ShrimpiaEssentials
 import network.shrimpia.essentials.util.AdventureExtension.asMiniMessage
@@ -22,17 +24,21 @@ class GreetingModule : ModuleBase(), Listener {
         }
     }
 
+    @Suppress("UnstableApiUsage")
     @EventHandler
     fun onPlayerJoin(ev: PlayerJoinEvent) {
         // Note: セットから削除できたということは、新規プレイヤーであるということ
         val isNewbie = newbieSet.remove(ev.player.uniqueId)
+        ev.player.playSound(
+            Sound.sound(if (isNewbie) SoundEventKeys.ENTITY_ITEM_PICKUP else SoundEventKeys.ENTITY_EXPERIENCE_ORB_PICKUP, Sound.Source.MASTER, 1.0f, 1.0f)
+        )
         ev.player.showTitle(
             Title.title(
                 "<#B93E43>Shrimpia Minecraft".asMiniMessage(),
                 if (isNewbie) {
-                    "ようこそ、<cyan>${ev.player.name}</cyan>さん！".asMiniMessage()
+                    "ようこそ、<aqua>${ev.player.name}</aqua>さん！".asMiniMessage()
                 } else {
-                    "おかえりなさい、<cyan>${ev.player.name}</cyan>さん！".asMiniMessage()
+                    "おかえりなさい、<aqua>${ev.player.name}</aqua>さん！".asMiniMessage()
                 },
                 Title.Times.times(Duration.ofSeconds(1L), Duration.ofSeconds(5L), Duration.ofSeconds(1L))
             )
